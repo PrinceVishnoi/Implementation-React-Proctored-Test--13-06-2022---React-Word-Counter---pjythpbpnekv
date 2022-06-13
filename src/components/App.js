336c6a9104a47a243755d66f5c731717930038e3
Prince Vishnoi <96827891+PrinceVishnoi@users.noreply.github.com>
2022-06-13 21:53:20 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import '../styles/App.css';
+import Counter from './Counter';
 const App = () => {
 
 
   return (
     <div id="main">
+     <Counter/>
     </div>
   )
 }
